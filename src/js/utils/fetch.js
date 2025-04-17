028dbb67e3117e3eac6b946f1346700eead5eb85
oelhaouc <126911890+elhaouchomar@users.noreply.github.com>
2025-04-17 11:13:14 +0100

    vqlid token

--- src/js/utils/fetch.js
+++ src/js/utils/fetch.js
@@ -1,8 +1,14 @@
 import { logout } from "../auth/logout.js";
 
 export async function recieveData(query) {
-  try {
     const JWT = localStorage.getItem("jwt");
+
+  if (!JWT || JWT.split('.').length !== 3) {
+    console.error("Invalid JWT:", JWT);
+    localStorage.removeItem("jwt");
+    logout();
+    return undefined;
+  try {
     const response = await fetch(
       "https://learn.zone01oujda.ma/api/graphql-engine/v1/graphql",
       {
